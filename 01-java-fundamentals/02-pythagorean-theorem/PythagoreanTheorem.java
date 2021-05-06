1994b3e19a29aedbd05c841200307f6c1bd9af3d
Michael Torres <mtorres11289@yahoo.com>
2021-05-05 22:47:44 -0400

    Pythagorean Theorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheorem.java
@@ -0,0 +1,6 @@
+import java.lang.Math;
+public class PythagoreanTheorem{
+    public double calculateHypotenuse(int legA, int legB){
+        return Math.sqrt((legA * legA) + (legB * legB));
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheoremTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTheoremTest{
+    public static void main(String[] args){
+        PythagoreanTheorem legC = new PythagoreanTheorem();
+        System.out.println(legC.calculateHypotenuse(4,6));
+    }
+}
