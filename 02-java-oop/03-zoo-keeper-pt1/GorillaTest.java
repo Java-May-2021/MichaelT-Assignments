5267f31311c95633b874509f55dbb2efb1206821
Michael Torres <mtorres11289@yahoo.com>
2021-06-03 23:03:42 -0400

    Zookeeper1

---
+++ 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
@@ -0,0 +1,13 @@
+public class GorillaTest{
+    public static void main(String[] args){
+        Gorilla koko = new Gorilla();
+
+        koko.displayEnergy();
+        koko.throwSomething();
+        koko.throwSomething();
+        koko.throwSomething();
+        koko.eatBanana();
+        koko.eatBanana();
+        koko.climb();  
+    }
+}
